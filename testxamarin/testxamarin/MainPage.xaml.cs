056358222af18326267472f1508f39757db7e190
ZkunCry <aksentev.03@mail.ru>
2023-03-24 00:32:39 +0500

    first commit

---
+++ testxamarin/testxamarin/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace testxamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        static readonly int[,] Colon = new int[5, 3]
+        {
+            { 0, 0,0 },
+            { 0, 1,0 },
+            { 0, 0,0 },
+            { 0, 1,0 },
+            { 0, 0,0 }
+        };
+        static readonly int[,,] numberPatterns = new int[10, 5, 3]
+    {
+        {
+            { 0, 1, 0}, { 1, 0, 1}, { 0,0,0}, { 1, 0,1},{ 0,1,0}
+        },
+        {
+            { 0,0,0}, { 0, 0,1}, { 0,0,0}, { 0, 0, 1},{ 0, 0,0}
+        },
+        {
+            { 0, 1,0}, { 0,0,1}, { 0, 1, 0}, { 1, 0,0},
+            { 0, 1,0 }
+        },
+        {
+            { 0,1,0}, 
+            {0,0,1}, 
+            { 0, 1, 0}, 
+            { 0, 0, 1},
+            { 0,1,0}
+        },
+        {
+            { 0, 0, 0}, 
+            { 1, 0, 1}, 
+            { 0, 1, 0}, 
+            { 0, 0, 1},
+            { 0,0,0}
+        },
+        {
+            { 0,1,0}, 
+            { 1, 0, 0}, 
+            { 0,1,0}, 
+            { 0,0,1},
+            { 0,1,0}
+        },
+        {
+            { 0,1,0}, 
+            { 1, 0, 0}, 
+            { 0, 1, 0}, 
+            { 1, 0, 1},
+            { 0,1,0}
+        },
+        {
+            { 0,1,0}, 
+            { 0, 0, 1}, 
+            { 0, 0, 0}, 
+            { 0, 0, 1},
+            { 0, 0, 0}
+        },
+        {
+            { 0, 1, 0}, 
+            { 1, 0, 1}, 
+            { 0, 1, 0}, 
+            { 1, 0, 1},
+            { 0, 1, 0}
+        },
+        {
+            { 0, 1, 0}, 
+            { 1, 0, 1}, 
+            { 0, 1, 0}, 
+            { 0, 0, 1},
+            { 0, 1, 0}
+        },
+    };
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
